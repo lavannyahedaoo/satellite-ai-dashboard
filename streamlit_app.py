358e341a52970d880e397bbaf52466e54d240291
lavannyahedaoo <lavannyahedaoo@gmail.com>
2025-12-30 19:49:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🚀 AI-Based Satellite Health Monitoring System")
+
+df = pd.read_csv("output_with_anomalies.csv")
+
+st.subheader("📊 Satellite Sensor Data")
+st.dataframe(df.head(20))
+
+st.subheader("📈 Satellite Health Parameters")
+st.line_chart(df[['battery', 'temperature', 'signal']])
+
+st.subheader("⚠️ Detected Anomalies")
+anomalies = df[df['anomaly'] == 'Anomaly']
+
+if len(anomalies) == 0:
+    st.success("No anomalies detected 🚀")
+else:
+    st.error(f"{len(anomalies)} anomalies detected!")
+    st.dataframe(anomalies)
